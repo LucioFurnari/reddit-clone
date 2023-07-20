bd21e643967616817157cd01fddaf3096ae7fc9d
LucioFurnari <luciofurnari99@gmail.com>
2023-07-20 18:46:31 -0300

    Import PostCard and Placeholder image, use multiple PostCard for placeholder

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,8 +1,13 @@
+import PostCard from "./PostCard"
+import PlaceHolderImage from "@/images/placeholder_post_image.jpg"
 
 function MainSection () {
   return (
-    <main>
-
+    <main className="flex flex-col  items-center max-h-[calc(100vh-52px)] w-full overflow-y-scroll gap-4 pt-6">
+      <PostCard votes={27} community="Cakes" user="CakeUser" time_passed={2} title="Look this cake!" content={PlaceHolderImage}/>
+      <PostCard votes={27} community="Cakes" user="CakeUser" time_passed={2} title="Look this cake!" content={PlaceHolderImage}/>
+      <PostCard votes={27} community="Cakes" user="CakeUser" time_passed={2} title="Look this cake!" content={PlaceHolderImage}/>
+      <PostCard votes={27} community="Cakes" user="CakeUser" time_passed={2} title="Look this cake!" content={PlaceHolderImage}/>
     </main>
   )
 }
