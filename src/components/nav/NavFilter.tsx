13125bb4724551c2265c168e7ebef81d291e5b6e
LucioFurnari <luciofurnari99@gmail.com>
2023-07-24 19:41:51 -0300

    Create and move nav filter to new component

---
+++ src/components/nav/NavFilter.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react"
+
+function NavFilter () {
+  return (
+    <fieldset className="flex items-center bg-[#272729] rounded-full border-[#323334] border-[1px] px-4 my-1 col-start-2 col-end-4 hover:bg-[#1c1c1d] hover:border-white">
+      <PhMagnifyingGlassLight />
+      <input className=" bg-inherit text-sm  w-full ml-1 outline-none " placeholder="Search Reddit"></input>
+    </fieldset>
+  )
+}
+
+export default NavFilter
+
+function PhMagnifyingGlassLight(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className=" w-6 h-auto py-2"  xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" {...props}><path fill="currentColor" d="m228.24 219.76l-51.38-51.38a86.15 86.15 0 1 0-8.48 8.48l51.38 51.38a6 6 0 0 0 8.48-8.48ZM38 112a74 74 0 1 1 74 74a74.09 74.09 0 0 1-74-74Z"></path></svg>
+  )
+}
