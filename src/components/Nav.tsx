114d04d4d493e791bb44a53c6cff9752f61d48ed
LucioFurnari <luciofurnari99@gmail.com>
2023-07-25 12:03:34 -0300

    Implement simple conditional rendering for online and offline user

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,6 @@
-import { SVGProps } from "react"
-import UserMenuOffline from "./nav_menus/UserMenuOffline"
+import NavOfflineFeed from "./nav/NavOfflineFeed"
+import NavOnlineFeed from "./nav/NavOnlineFeed"
+import NavFilter from "./nav/NavFilter"
 
 function Nav () {
   return (
@@ -10,40 +11,14 @@ function Nav () {
           <svg  className="w-14 inline" viewBox="0 0 57 18" xmlns="http://www.w3.org/2000/svg"><g fill="#D7DADC"><path d="M54.63,16.52V7.68h1a1,1,0,0,0,1.09-1V6.65a1,1,0,0,0-.93-1.12H54.63V3.88a1.23,1.23,0,0,0-1.12-1.23,1.2,1.2,0,0,0-1.27,1.11V5.55h-1a1,1,0,0,0-1.09,1v.07a1,1,0,0,0,.93,1.12h1.13v8.81a1.19,1.19,0,0,0,1.19,1.19h0a1.19,1.19,0,0,0,1.25-1.12A.17.17,0,0,0,54.63,16.52Z"></path><circle fill="#FF4500" cx="47.26" cy="3.44" r="2.12"></circle><path d="M48.44,7.81a1.19,1.19,0,1,0-2.38,0h0v8.71a1.19,1.19,0,0,0,2.38,0Z"></path><path d="M30.84,1.19A1.19,1.19,0,0,0,29.65,0h0a1.19,1.19,0,0,0-1.19,1.19V6.51a4.11,4.11,0,0,0-3-1.21c-3.1,0-5.69,2.85-5.69,6.35S22.28,18,25.42,18a4.26,4.26,0,0,0,3.1-1.23,1.17,1.17,0,0,0,1.47.8,1.2,1.2,0,0,0,.85-1.05ZM25.41,15.64c-1.83,0-3.32-1.77-3.32-4s1.48-4,3.32-4,3.31,1.78,3.31,4-1.47,3.95-3.3,3.95Z"></path><path d="M43.28,1.19A1.19,1.19,0,0,0,42.09,0h0a1.18,1.18,0,0,0-1.18,1.19h0V6.51a4.15,4.15,0,0,0-3-1.21c-3.1,0-5.69,2.85-5.69,6.35S34.72,18,37.86,18A4.26,4.26,0,0,0,41,16.77a1.17,1.17,0,0,0,1.47.8,1.19,1.19,0,0,0,.85-1.05ZM37.85,15.64c-1.83,0-3.31-1.77-3.31-4s1.47-4,3.31-4,3.31,1.78,3.31,4-1.47,3.95-3.3,3.95Z"></path><path d="M17.27,12.44a1.49,1.49,0,0,0,1.59-1.38v-.15a4.81,4.81,0,0,0-.1-.85A5.83,5.83,0,0,0,13.25,5.3c-3.1,0-5.69,2.85-5.69,6.35S10.11,18,13.25,18a5.66,5.66,0,0,0,4.39-1.84,1.23,1.23,0,0,0-.08-1.74l-.11-.09a1.29,1.29,0,0,0-1.58.17,3.91,3.91,0,0,1-2.62,1.12A3.54,3.54,0,0,1,10,12.44h7.27Zm-4-4.76a3.41,3.41,0,0,1,3.09,2.64H10.14A3.41,3.41,0,0,1,13.24,7.68Z"></path><path d="M7.68,6.53a1.19,1.19,0,0,0-1-1.18A4.56,4.56,0,0,0,2.39,6.91V6.75A1.2,1.2,0,0,0,0,6.75v9.77a1.23,1.23,0,0,0,1.12,1.24,1.19,1.19,0,0,0,1.26-1.1.66.66,0,0,0,0-.14v-5A3.62,3.62,0,0,1,5.81,7.7a4.87,4.87,0,0,1,.54,0h.24A1.18,1.18,0,0,0,7.68,6.53Z"></path></g></svg>
         </a>
       </div>
-      <fieldset className="flex items-center bg-[#272729] rounded-full border-[#323334] border-[1px] px-4 my-1 col-start-2 col-end-4 hover:bg-[#1c1c1d] hover:border-white">
-        <PhMagnifyingGlassLight />
-        <input className=" bg-inherit text-sm  w-full ml-1 outline-none " placeholder="Search Reddit"></input>
-      </fieldset>
-      <div className="flex justify-end items-center relative">
-        <button className="bg-[#ff4500] hover:bg-[#ff5415] text-[#ffffff] px-10 py-1 text-center  rounded-full font-semibold">Log In</button>
-        <button className="flex items-center ml-5 py-1 px-2 rounded-md border-[1px] border-transparent hover:border-[#323334] peer">
-            <ClarityUserLine />
-            <IcBaselineKeyboardArrowDown />
-        </button>
-        <UserMenuOffline />
-      </div>
+      <NavFilter />
+      {true ? 
+        <NavOnlineFeed />
+        :
+        <NavOfflineFeed />
+      }
     </nav>
   )
 }
 
 export default Nav
-
-export function PhMagnifyingGlassLight(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg className=" w-6 h-auto py-2"  xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" {...props}><path fill="currentColor" d="m228.24 219.76l-51.38-51.38a86.15 86.15 0 1 0-8.48 8.48l51.38 51.38a6 6 0 0 0 8.48-8.48ZM38 112a74 74 0 1 1 74 74a74.09 74.09 0 0 1-74-74Z"></path></svg>
-  )
-}
-
-
-export function ClarityUserLine(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg className="w-6 h-auto block" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 36 36" {...props}><path fill="currentColor" d="M18 17a7 7 0 1 0-7-7a7 7 0 0 0 7 7Zm0-12a5 5 0 1 1-5 5a5 5 0 0 1 5-5Z" className="clr-i-outline clr-i-outline-path-1"></path><path fill="currentColor" d="M30.47 24.37a17.16 17.16 0 0 0-24.93 0A2 2 0 0 0 5 25.74V31a2 2 0 0 0 2 2h22a2 2 0 0 0 2-2v-5.26a2 2 0 0 0-.53-1.37ZM29 31H7v-5.27a15.17 15.17 0 0 1 22 0Z" className="clr-i-outline clr-i-outline-path-2"></path><path fill="none" d="M0 0h36v36H0z"></path></svg>
-  )
-}
-
-
-export function IcBaselineKeyboardArrowDown(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg className="w-6 h-auto  block" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6l1.41-1.41z"></path></svg>
-  )
-}
