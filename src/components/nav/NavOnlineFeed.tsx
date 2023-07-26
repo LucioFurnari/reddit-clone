c2373cd4d6aea2586735f849f768b9955e71ea43
LucioFurnari <luciofurnari99@gmail.com>
2023-07-26 12:12:10 -0300

    Finish the style

--- src/components/nav/NavOnlineFeed.tsx
+++ src/components/nav/NavOnlineFeed.tsx
@@ -29,19 +29,29 @@ function UserProfile (props: {name:string, img:StaticImageData | string}) {
 
 function CreatePostButton () {
   return (
-    <button className='hover:bg-[#232324] p-1'>
+    <button className='hover:bg-[#232324] p-1 mr-2'>
       <IcOutlineAdd />
     </button>
   )
 } 
 
+function CreateCommunityButton () {
+  return (
+    <button className='hover:bg-[#232324] p-2 text-sm  text-start'>
+      <IcOutlineAdd />
+      <span className='ml-2'>Create Community</span>
+    </button>
+  )
+}
+
 function UserProfileMenu () {
   return (
-    <div className='peer-focus:h-auto h-0 overflow-hidden absolute top-12 right-0 bg-[#1a1a1b] peer-focus:border-[1px] border-[#343536] rounded-md'>
-      <button className='p-2 hover:bg-[#232324]'>
+    <div className='flex flex-col min-w-[12rem] peer-focus:h-auto h-0 overflow-hidden absolute top-12 right-0 bg-[#1a1a1b] peer-focus:border-[1px] border-[#343536] rounded-md'>
+      <button className='p-2 hover:bg-[#232324] text-sm text-start'>
         <IcTwotoneLogIn />
-        Log Out
+        <span className='ml-2'>Log Out</span>
       </button>
+      <CreateCommunityButton />
     </div>
   )
 }
@@ -56,7 +66,7 @@ function IcBaselineKeyboardArrowDown(props: SVGProps<SVGSVGElement>) {
 
 function IcOutlineAdd(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg className='w-8 h-auto inline' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path></svg>
+    <svg className='w-[1.3rem] h-auto inline' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path></svg>
   )
 }
 
