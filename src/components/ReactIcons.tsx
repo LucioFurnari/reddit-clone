2745cc953bc83063bb9a35b5c6f78d95decfc858
LucioFurnari <luciofurnari99@gmail.com>
2023-07-26 16:41:03 -0300

    Save all react svg icons in this file

---
+++ src/components/ReactIcons.tsx
@@ -0,0 +1,31 @@
+import { SVGProps } from "react"
+
+export function IcBaselineKeyboardArrowDown(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className="w-6 h-auto inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6l1.41-1.41z"></path></svg>
+  )
+}
+
+export function IcOutlineAdd(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className='w-[1.3rem] h-auto inline' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path></svg>
+  )
+}
+
+export function IcTwotoneLogIn(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className="fill-white inline" xmlns="http://www.w3.org/2000/svg" width="1.3rem" height="auto" viewBox="0 0 24 24" {...props}><path d="M9 2h9c1.1 0 2 .9 2 2v16c0 1.1-.9 2-2 2H9c-1.1 0-2-.9-2-2v-2h2v2h9V4H9v2H7V4c0-1.1.9-2 2-2z"></path><path fill="currentColor" d="M10.09 15.59L11.5 17l5-5l-5-5l-1.41 1.41L12.67 11H3v2h9.67z"></path></svg>
+  )
+}
+
+export function PhMagnifyingGlassLight(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className=" w-6 h-auto py-2"  xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" {...props}><path fill="currentColor" d="m228.24 219.76l-51.38-51.38a86.15 86.15 0 1 0-8.48 8.48l51.38 51.38a6 6 0 0 0 8.48-8.48ZM38 112a74 74 0 1 1 74 74a74.09 74.09 0 0 1-74-74Z"></path></svg>
+  )
+}
+
+export function IonMdHome(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className=" w-[1.6rem] h-auto inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}><path d="M208 448V320h96v128h97.6V256H464L256 64 48 256h62.4v192z" fill="currentColor"></path></svg>
+  )
+}
