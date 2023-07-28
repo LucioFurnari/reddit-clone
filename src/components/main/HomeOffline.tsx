4255c4d0fa46209287e68afaff6e53c270b2ecf9
LucioFurnari <luciofurnari99@gmail.com>
2023-07-28 01:28:23 -0300

    Create home online and offline component

---
+++ src/components/main/HomeOffline.tsx
@@ -0,0 +1,13 @@
+import HomeAside from "./HomeAside"
+import MainSection from "./MainSection"
+
+function HomeOffline () {
+  return (
+    <div className=" flex">
+      <HomeAside />
+      <MainSection />
+    </div>  
+  )
+}
+
+export default HomeOffline
---
+++ src/components/main/HomeOnline.tsx
@@ -0,0 +1,11 @@
+import MainSection from "./MainSection"
+
+function HomeOnline () {
+  return (
+    <div>
+      <MainSection />
+    </div>  
+  )
+}
+
+export default HomeOnline
