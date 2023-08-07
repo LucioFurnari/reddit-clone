b2cbfb6d51b0d7ff19425a5a47367f424cc88506
LucioFurnari <luciofurnari99@gmail.com>
2023-08-07 19:20:35 -0300

    Create CommunityMain component

---
+++ src/components/community/CommunityMain.tsx
@@ -0,0 +1,11 @@
+import CommunityAside from "./CommunityAside"
+
+function CommunityMain () {
+  return (
+    <main className="grid grid-cols-6 pt-4">
+      <CommunityAside />
+    </main>
+  )
+}
+
+export default CommunityMain;
