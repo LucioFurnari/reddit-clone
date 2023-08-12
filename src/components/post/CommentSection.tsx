daa8c186e65f5ae9c4d7d8222e626e2cf080e573
LucioFurnari <luciofurnari99@gmail.com>
2023-08-11 23:36:50 -0300

    Style form textarea and submit button

--- src/components/post/CommentSection.tsx
+++ src/components/post/CommentSection.tsx
@@ -5,8 +5,8 @@ function CommentSection () {
       <fieldset className="p-4">
         <p className="mb-2">Comment as User</p>
         <form className="flex flex-col">
-          <textarea className=" max-h-40 h-40 bg-transparent border-[1px] border-[var(--border-gray)] rounded"></textarea>
-          <button className="ml-auto bg-[var(--button-bg-white)] text-black px-4 mt-4 rounded-full">Comment</button>
+          <textarea className="pl-4 pt-2 max-h-40 h-40 bg-transparent border-[1px] border-[var(--border-gray)] focus:border-white focus:outline-none rounded" placeholder="What are your thoughts?"></textarea>
+          <button className="ml-auto bg-[var(--button-bg-white)] hover:bg-[var(--button-hover-gray)] text-black px-4 mt-4 rounded-full">Comment</button>
         </form>
       </fieldset>
     </article>
