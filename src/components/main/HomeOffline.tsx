8e5d76d38e97acc90629d0f6da4a603123681034
LucioFurnari <luciofurnari99@gmail.com>
2023-08-14 20:12:41 -0300

    Make the offline home more responsive

--- src/components/main/HomeOffline.tsx
+++ src/components/main/HomeOffline.tsx
@@ -3,7 +3,7 @@ import MainSection from "./MainSection"
 
 function HomeOffline () {
   return (
-    <main className="grid grid-cols-6 pt-12">
+    <main className="grid grid-cols-1 px-4 lg:px-0 lg:grid-cols-4 xl:grid-cols-6 pt-12">
       <HomeAside />
       <MainSection />
     </main>  
