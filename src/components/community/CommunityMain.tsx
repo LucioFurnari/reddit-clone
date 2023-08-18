f5339212b1f31b49d5de3238e82a17723b57e0a7
LucioFurnari <luciofurnari99@gmail.com>
2023-08-17 23:19:05 -0300

    Remove community post container and replace it with PostContainer component

--- src/components/community/CommunityMain.tsx
+++ src/components/community/CommunityMain.tsx
@@ -1,8 +1,12 @@
 import CommunityAside from "./CommunityAside"
+import PostContainer from "../post/PostContainer";
 
 function CommunityMain () {
   return (
-    <main className="grid grid-cols-7 pt-4">
+    <main className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-[1fr_1fr_1fr_1fr_1fr_200px_1fr] xl:grid-cols-[1fr_1fr_1fr_1fr_1fr_300px_1fr] pt-4">
+      <div className="grid md:col-start-1 md:col-end-4 lg:col-start-2 lg:col-end-5 xl:col-start-2 xl:col-end-5 grid-cols-1">
+        <PostContainer />
+      </div>
       <CommunityAside />
     </main>
   )
