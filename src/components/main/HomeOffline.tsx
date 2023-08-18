3916f8d0ea854cccb0e45c5226c287ff28084e9a
LucioFurnari <luciofurnari99@gmail.com>
2023-08-18 20:23:07 -0300

    Add div tag to contain PostContainer and improve responsive design

--- src/components/main/HomeOffline.tsx
+++ src/components/main/HomeOffline.tsx
@@ -1,11 +1,13 @@
-import HomeAside from "./HomeAside"
+import HomeAside from "./AsideHomeOffline"
 import PostContainer from "../post/PostContainer"
 
 function HomeOffline () {
   return (
-    <main className="grid grid-cols-1 lg:grid-cols-4 xl:grid-cols-5 pt-32 lg:pt-20">
+    <main className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 pt-32 lg:pt-20">
       <HomeAside />
-      <PostContainer />
+      <div className=" md:col-start-2 md:col-end-4 lg:col-start-2 lg:col-end-4 xl:col-start-3 xl:col-end-5">
+        <PostContainer />
+      </div>
     </main>  
   )
 }
