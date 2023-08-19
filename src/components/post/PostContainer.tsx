3f7f4d08dbcbec27c583c4ce1344d147e42ca6c2
LucioFurnari <luciofurnari99@gmail.com>
2023-08-18 21:48:22 -0300

    Remove and comment the grid rules

--- src/components/post/PostContainer.tsx
+++ src/components/post/PostContainer.tsx
@@ -3,7 +3,7 @@ import PlaceHolderImage from "@/images/placeholder_post_image.jpg"
 
 function PostContainer (props: {}) {
   return (
-    <section className="grid grid-cols-1  md:col-start-1 lg:col-start-2 md:col-end-4 gap-4 ">
+    <section className="grid grid-cols-1 gap-4 ">
       <PostCard votes={27} community="Cakes" user="CakeUser" time_passed={2} title="Look this cake!" content={PlaceHolderImage}/>
       <PostCard votes={27} community="Cakes" user="CakeUser" time_passed={2} title="Look this cake!" content={PlaceHolderImage}/>
       <PostCard votes={27} community="Cakes" user="CakeUser" time_passed={2} title="Look this cake!" content={PlaceHolderImage}/>
@@ -13,3 +13,5 @@ function PostContainer (props: {}) {
 }
 
 export default PostContainer;
+
+// md:col-start-1 lg:col-start-2 md:col-end-4 xl:col-start-3 xl:col-end-5
