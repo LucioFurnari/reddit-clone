acf4ef9dd675714045d6dc16dbba8b5b47bdc00f
LucioFurnari <luciofurnari99@gmail.com>
2023-08-24 23:22:58 -0300

    Change style of the component

--- src/components/login/LoginPage.tsx
+++ src/components/login/LoginPage.tsx
@@ -1,11 +1,13 @@
 
 function LoginPage () {
   return (
-    <section className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <section className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-10 rounded-lg">
       <fieldset>
+        <h2 className="text-black">Log In</h2>
         <form className="flex flex-col">
-          <input type="text" placeholder="Name"></input>
-          <input type="text" placeholder="Password"></input>
+          <input className="bg-gray-100 mt-2 p-2 rounded-full" type="text" placeholder="Name"></input>
+          <input className="bg-gray-100 mt-2 p-2 rounded-full" type="text" placeholder="Password"></input>
+          <button className="bg-[--button-orange] mt-2 py-1 rounded-full">Log In</button>
         </form>
       </fieldset>
     </section>
